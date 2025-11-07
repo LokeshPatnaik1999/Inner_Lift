f22a48f6294b23f77842a71e4950d20ff285417d
LOKESH KUMAR PATNAIK <lokeshbabu944@gmail.com>
2025-11-07 18:47:35 -0500

    Wrap App component with BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -49,8 +49,12 @@ const router = createBrowserRouter(
   }
 )
 
-createRoot(document.getElementById('root')).render(
+import { BrowserRouter } from 'react-router-dom';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}> // <-- ADD THIS LINE
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 )
